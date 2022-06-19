9ea0a2b3c15b3709c236286d712e2ecbbb146cb4
Liuhaiqing <957443837@qq.com>
2022-06-19 10:49:43 +0800

    创建retrofit接口、请求体以及响应体

--- app/src/main/java/com/lhq/sunnyweatherapp/logic/network/PlaceService.kt
+++ app/src/main/java/com/lhq/sunnyweatherapp/logic/network/PlaceService.kt
@@ -1,8 +1,13 @@
 package com.lhq.sunnyweatherapp.logic.network
 
+
+import com.lhq.sunnyweatherapp.SunnyWeatherApplication
+import com.lhq.sunnyweatherapp.logic.model.PlaceResponse
+import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 internal interface PlaceService {
-    @GET("v2/place?token=\${SunnyWeatherApplication.TOKEN}&lang=zh_CN")
-    fun searchPlaces(): `fun`?
+    @GET("v2/place?token=${SunnyWeatherApplication.TOKEN}&lang=zh_CN")
+    fun searchPlaces(@Query("query") query: String): Call<PlaceResponse>
 }
